869085e4969016e1438b799ff91dbdd2f176038e
manchesterkent <manchesterkent@gmail.com>
2018-01-19 14:49:11 +0000

    增加了手机端的退出和登录

--- src/js/components/mobile_header.js
+++ src/js/components/mobile_header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import 'antd/dist/antd.css';
-
+import {Link,Router,Route} from 'react-router';
 import {
 	Menu,
 	Icon,
@@ -27,9 +27,19 @@ class MobileHeader extends React.Component {
 			action: 'login',
 			hasLogined: false,
 			userNickName: '',
-			userid: 0
+			userid: ''
 		};
 	}
+
+	componentWillMount(){
+			if (localStorage.userid!='') {
+				this.setState({hasLogined:true});
+				this.setState({userNickName:localStorage.userNickName,userid:localStorage.userid});
+			}
+			console.log(localStorage.userNickName+" userNickName ");
+			console.log(localStorage.userid + "UserId");
+		};
+
 setModalVisible(value){
    this.setState({
      modalVisible:value
@@ -49,23 +59,42 @@ handleSubmit(event){
       method : 'GET'
   }
   var formData= this.props.form.getFieldsValue();
-  fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName="+formData.r_userName+"&r_password="+formData.r_password+"&r_confirmPassword="+formData.r_confirmPassword,myFetchOptions).
+	console.log(formData);
+	fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
+	+ "&username="+formData.userName+"&password="+formData.password
+	+"&r_userName=" + formData.r_userName + "&r_password="
+	+ formData.r_password + "&r_confirmPassword="
+	+ formData.r_confirmPassword, myFetchOptions).
   		then(response=>response.json()).then(json=>{
   			this.setState({userNickName:json.NickUserName,userid:json.UserId});
+				localStorage.userNickName = json.NickUserName;
+				localStorage.userid = json.UserId;
   		});
   		message.success("请求成功！");
+			this.setState({hasLogined : true});
   		this.setModalVisible(false);
 };
 login(){
   this.setModalVisible(true);
 };
 
+callback(key){
+	if(key == 1){
+		this.setState({action : "login"});
+	}else if(key == 2){
+	  this.setState({action : "register"});
+	}
+}
+logout(){
+	localStorage.userNickName = '';
+	localStorage.userid = '';
+	this.setState({hasLogined : false});
+}
+
 render() {
 	let {getFieldDecorator} = this.props.form;
 	const userShow = this.state.hasLogined ?
-	<Link>
-		<Icon type="inbox"/>
-	</Link>
+		<Icon type="poweroff" onClick = {this.logout.bind(this)}/>
 	:
 	<Icon type="smile" onClick={this.login.bind(this)}/>
 		return (
@@ -77,7 +106,27 @@ render() {
         </header>
 				<Modal  title="用户中心" wrapClassName="vertical-center-modal"  visible={this.state.modalVisible}  onOk={() => this.setModalVisible(false)}
 				onCancel={()=>this.setModalVisible(false)} okText = "关闭">
-						<Tabs type="card">
+						<Tabs type="card" onChange = {this.callback.bind(this)}>
+						<TabPane tab="登录" key="1">
+							<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+								<FormItem label="账户">
+														{getFieldDecorator('userName', {
+																 rules: [{ required: true, message: "请填写用户名" }],
+															 })(
+																 <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入您的用户名" />
+															 )}
+									{/*<Input placeholder="请输入您的账号" {...getFieldDecorator('r_userName')}/>*/}
+								</FormItem>
+								<FormItem label="密码">
+														{getFieldDecorator('password', {
+															 rules: [{ required: true, message: "请输入您的密码" }],
+														 })(
+															 <Input type = "password" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入您的密码" />
+														 )}
+								</FormItem>
+								<Button type="primary" htmlType="submit" >确认登录</Button>
+							</Form>
+						</TabPane>
 								<TabPane tab="注册" key="2">
 									<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
 										<FormItem label="账户">
